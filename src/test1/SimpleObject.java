2341ecccc65ab9c4718085c11a6cc3b981cf8062
Peter Nielsen <nielsenp@comcast.net>
2009-06-10 22:27:22 -0500

    first commit

---
+++ src/test1/SimpleObject.java
@@ -0,0 +1,22 @@
+package test1;
+
+public class SimpleObject {
+	
+	private String varString = "myStringVal";
+	private Integer varInteger = new Integer(5);
+	public String getVarString() {
+		return varString;
+	}
+	public void setVarString(String varString) {
+		this.varString = varString;
+	}
+	public Integer getVarInteger() {
+		return varInteger;
+	}
+	public void setVarInteger(Integer varInteger) {
+		this.varInteger = varInteger;
+	}
+	
+	
+
+}
